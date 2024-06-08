6e58a49ec973ec40f58eae1c6ff02be7096ccf09
Victor V B Vargas <vvbvargas@Victors-MacBook-Air.local>
2024-06-08 14:50:33 -0300

    fix: url after code changes

--- src/main/webapp/components/editor/url-input/editor-url-input.js
+++ src/main/webapp/components/editor/url-input/editor-url-input.js
@@ -1,53 +1,62 @@
 /**********************
-* Editor URL Input JS *
-***********************/
+ * Editor URL Input JS *
+ ***********************/
 
-const { setUrlValue, initEditorUrlInput } = (function() {
-  function setUrlValue(
-    url=undefined,
-    { encodedDiagram=undefined, index=undefined } = {},
-    { suppressEditorChangedMessage=false } = {}
+const { setUrlValue, initEditorUrlInput } = (function () {
+  function setUrlValue (
+    url = undefined,
+    { encodedDiagram = undefined, index = undefined } = {},
+    { suppressEditorChangedMessage = false } = {}
   ) {
-    if (!url && !encodedDiagram) return;
+    if (!url && !encodedDiagram) return
     if (suppressEditorChangedMessage) {
-      suppressNextMessage("url");
+      suppressNextMessage('url')
     }
-    document.getElementById("url").value = url ? url : resolvePath(buildUrl("png", encodedDiagram, index));
+    document.getElementById('url').value = url
+      ? url
+      : resolvePath(buildUrl('uml', encodedDiagram, index))
   }
 
-  function initEditorUrlInput() {
-    const input = document.getElementById("url");
+  function initEditorUrlInput () {
+    const input = document.getElementById('url')
 
-    function copyUrlToClipboard() {
-      input.focus();
-      input.select();
-      navigator.clipboard?.writeText(input.value).catch(() => {});
+    function copyUrlToClipboard () {
+      input.focus()
+      input.select()
+      navigator.clipboard?.writeText(input.value).catch(() => {})
     }
-    async function onInputChanged(event) {
-      document.appConfig.autoRefreshState = "started";
-      event.target.title = event.target.value;
-      const analysedUrl = analyseUrl(event.target.value);
+    async function onInputChanged (event) {
+      document.appConfig.autoRefreshState = 'started'
+      event.target.title = event.target.value
+      const analysedUrl = analyseUrl(event.target.value)
       // decode diagram (server request)
-      const code = await makeRequest("GET", "coder/" + analysedUrl.encodedDiagram);
+      const code = await makeRequest(
+        'GET',
+        'coder/' + analysedUrl.encodedDiagram
+      )
       // change editor content without sending the editor change message
-      setEditorValue(document.editor, code, { suppressEditorChangedMessage: true });
+      setEditorValue(document.editor, code, {
+        suppressEditorChangedMessage: true
+      })
       sendMessage({
-        sender: "url",
+        sender: 'url',
         data: {
           encodedDiagram: analysedUrl.encodedDiagram,
-          index: analysedUrl.index,
+          index: analysedUrl.index
         },
-        synchronize: true,
-      });
+        synchronize: true
+      })
     }
 
     // resolve relative path inside url input once
-    setUrlValue(resolvePath(input.value));
+    setUrlValue(resolvePath(input.value))
     // update editor and everything else if the URL input is changed
-    input.addEventListener("change", onInputChanged);
+    input.addEventListener('change', onInputChanged)
     // add listener
-    document.getElementById("url-copy-btn").addEventListener("click", copyUrlToClipboard);
+    document
+      .getElementById('url-copy-btn')
+      .addEventListener('click', copyUrlToClipboard)
   }
 
-  return { setUrlValue, initEditorUrlInput };
-})();
+  return { setUrlValue, initEditorUrlInput }
+})()
